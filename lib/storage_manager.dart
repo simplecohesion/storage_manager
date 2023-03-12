9c659597833a3d998e446b4c8b59a2289c742820
Roscoe <roscoe@simplecohesion.com>
2023-03-12 15:09:06 +1100

    remove dio package, update download_manager package

--- lib/storage_manager.dart
+++ lib/storage_manager.dart
@@ -3,6 +3,5 @@ library storage_manager;
 export 'controllers/storage_manager_controller.dart';
 export 'enums/storage_manager_status.dart';
 export 'models/storage_manager_snapshot.dart';
-export 'enums/storage_manager_status.dart';
 export 'core/download_manager.dart';
 export 'widgets/storage_manager_builder.dart';
